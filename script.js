698e0dabc070a4a367fefde487a6e683dd482ed4
Ar1f007 <muhammadarif9416@yahoo.com>
2021-04-07 20:45:39 +0600

    team section styling

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 "use strict";
+document.querySelectorAll(".story-btn").forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    btn.classList.toggle("change");
+    btn.nextElementSibling.classList.toggle("change");
+  });
+});
